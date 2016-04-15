dc2cc9e65cbaf4922c6ec1bc8dccf7893aeea435
Amy Jennings <amyjenningsmn@gmail.com>
2016-04-15 11:34:11 -0500

    replaced  with this using ParentController as parent

--- client/client.js
+++ client/client.js
@@ -1,15 +1,16 @@
 var app = angular.module('blueApp', []);
 
-app.controller('BlueController', ['$scope', '$http', function($scope, $http){
-  $scope.user = {};
-  $scope.userList = [];
+app.controller('ParentController', ['$http',function($http){
+  var parent = this;
+  parent.user = {};
+  parent.userList = [];
 
-  $scope.getPeople = function() {
+  parent.getPeople = function() {
     console.log('getpeople ran');
     $http.get('/create').then(function(response){
       console.log(response.data);
-      $scope.userList = [];
-      $scope.userList = response.data;
+      parent.userList = [];
+      parent.userList = response.data;
 
       if (response.status !== 200){
         console.log('Error on getting data')
@@ -23,12 +24,46 @@ app.controller('BlueController', ['$scope', '$http', function($scope, $http){
     });
   };
 
-  $scope.deletePerson = function(person, index){
+  parent.deletePerson = function(person, index){
     console.log("index", index);
     var id = person._id;
     $http.delete('/delete/' + id).then(function(response){
       console.log('ajax delete call made');
     });
-    $scope.userList.splice(index, 1);
+    parent.userList.splice(index, 1);
   }
 }])
+
+// [[[[[[[[[[[[[[[[[[ Old Code ]]]]]]]]]]]]]]]]]]
+// app.controller('BlueController', ['$scope', '$http', function($scope, $http){
+//   $scope.user = {};
+//   $scope.userList = [];
+//
+//   $scope.getPeople = function() {
+//     console.log('getpeople ran');
+//     $http.get('/create').then(function(response){
+//       console.log(response.data);
+//       $scope.userList = [];
+//       $scope.userList = response.data;
+//
+//       if (response.status !== 200){
+//         console.log('Error on getting data')
+//       }
+//       // $scope.user = {};
+//       // $scope.userList = response.data;
+//       // return response.data;
+//
+//       console.log(response.data);
+//
+//     });
+//   };
+//
+//   $scope.deletePerson = function(person, index){
+//     console.log("index", index);
+//     var id = person._id;
+//     $http.delete('/delete/' + id).then(function(response){
+//       console.log('ajax delete call made');
+//     });
+//     $scope.userList.splice(index, 1);
+//   }
+// }])
